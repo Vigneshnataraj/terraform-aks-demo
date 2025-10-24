319c0e819812e99809c0d0b810a477a61c21f26d
Vigneshnataraj <75323848+Vigneshnataraj@users.noreply.github.com>
2025-10-24 20:55:16 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,5 +1,4 @@
-variable "client_id" {}
-variable "client_secret" {}
-variable "tenant_id" {}
-variable "subscription_id" {}
-
+client_id = "${secrets.getValue("ARM_CLIENT_ID")}"
+client_secret = "${secrets.getValue("ARM_CLIENT_SECRET")}"
+tenant_id = "${secrets.getValue("ARM_TENANT_ID")}"
+subscription_id = "${secrets.getValue("ARM_SUBSCRIPTION_ID")}"

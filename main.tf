9ce5b9274285a0dd66fa8c799efb7fa8ada42742
Vignesh Prabhu N (HCL Technologies Corporate Services) <v-vignn@microsoft.com>
2025-10-24 20:07:57 +0530

    Initial commit for AKS

---
+++ main.tf
@@ -0,0 +1,22 @@
+resource "azurerm_resource_group" "aks_rg" {
+  name     = "myAKSResourceGroup"
+  location = "East US"
+}
+
+resource "azurerm_kubernetes_cluster" "aks_cluster" {
+  name                = "myAKSCluster"
+  location            = azurerm_resource_group.aks_rg.location
+  resource_group_name = azurerm_resource_group.aks_rg.name
+  dns_prefix          = "myaksdns"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 2
+    vm_size    = "Standard_DS2_v2"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+}
+
